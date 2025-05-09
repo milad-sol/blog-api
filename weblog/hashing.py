f92e3de81109eb6f71836c8c734b1ee68482d465
milad-sol <lvlilad_soltani@yahoo.com>
2025-05-09 13:02:06 +0330

    delete files

--- weblog/hashing.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-
-
-class Hash:
-    def bcrypt( password: str):
-        hashed_password = pwd_context.hash(password)
-        return hashed_password
